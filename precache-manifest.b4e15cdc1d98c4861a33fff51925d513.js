c905a9e00903e551d13cb16fc628bf401676e317
ikerismak <ikerismak@gmail.com>
2023-02-15 15:06:05 -0600

    Updates

---
+++ precache-manifest.b4e15cdc1d98c4861a33fff51925d513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d238be1bdaf3f4cadfe",
+    "url": "/TodoApp/static/css/main.3d9e2ba4.chunk.css"
+  },
+  {
+    "revision": "6d238be1bdaf3f4cadfe",
+    "url": "/TodoApp/static/js/main.6d238be1.chunk.js"
+  },
+  {
+    "revision": "bee36d96ecf68392c5e3",
+    "url": "/TodoApp/static/js/1.bee36d96.chunk.js"
+  },
+  {
+    "revision": "d779971c9701e8dcfd5b",
+    "url": "/TodoApp/static/js/runtime~main.d779971c.js"
+  },
+  {
+    "revision": "884b48a6023329b88c005453f0c1dbdb",
+    "url": "/TodoApp/static/media/notebook.884b48a6.gif"
+  },
+  {
+    "revision": "2054958e5204329b8e30bbdf745e0e31",
+    "url": "/TodoApp/index.html"
+  }
+];
